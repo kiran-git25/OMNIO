054511a1d98d6bf7782b2fd6e4cffd1410dbed1f
kiran-git25 <kiran-sai@outlook.com>
2025-08-08 23:36:29 +0530

    Delete src/components/viewers/ImageViewer.jsx

--- src/components/viewers/ImageViewer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function ImageViewer({ file }) {
-  const url = URL.createObjectURL(file);
-  return <img src={url} alt="Uploaded" style={{ maxWidth: '100%', marginTop: '1rem' }} />;
-}
