8fea201b4e6b5244e3ec206b87e18ffd690c800a
kiran-git25 <kiran-sai@outlook.com>
2025-08-08 23:36:45 +0530

    Delete src/components/viewers/PDFViewer.jsx

--- src/components/viewers/PDFViewer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function PDFViewer({ file }) {
-  const url = URL.createObjectURL(file);
-  return <iframe src={url} width="100%" height="600px" title="PDF Viewer" />;
-}
