d7772df454cf94a9d3a8b8c3522095f80cc4b8d7
kiran-git25 <kiran-sai@outlook.com>
2025-08-09 20:49:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import React, { useRef } from "react";
-import FileBrowser from "./components/FileBrowser";
-import TabManager from "./components/TabManager";
+import React, { useState } from "react";
+import ViewerPanel from "./components/ViewerPanel";
 import ChatBox from "./components/ChatBox";
 
 export default function App() {
-  const tabManagerRef = useRef();
+  const [activeFile, setActiveFile] = useState(null);
 
   return (
-    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
-      {/* File Upload / Paste Bar */}
-      <FileBrowser
-        onFileOpen={(name, url) => tabManagerRef.current?.openFile(name, url)}
-      />
-
-      {/* Main split view */}
-      <div style={{ flex: 1, display: "flex" }}>
-        {/* Tabs + Viewers */}
-        <div style={{ flex: 3, display: "flex", flexDirection: "column" }}>
-          <TabManager ref={tabManagerRef} />
-        </div>
-
-        {/* Chat */}
-        <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-          <ChatBox />
-        </div>
-      </div>
+    <div style={{ display: "flex", height: "100vh" }}>
+      <ViewerPanel activeFile={activeFile} />
+      <ChatBox onOpenFile={setActiveFile} />
     </div>
   );
 }
