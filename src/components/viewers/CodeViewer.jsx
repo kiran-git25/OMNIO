3cb6827f87799ff2bab8fed0faa7161b2b2eb848
kiran-git25 <kiran-sai@outlook.com>
2025-08-09 07:09:36 +0530

    Update CodeViewer.jsx

--- src/components/viewers/CodeViewer.jsx
+++ src/components/viewers/CodeViewer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/themes/prism.css';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-python';
 
