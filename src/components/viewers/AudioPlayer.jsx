de5267cbac619fe8fbd2994ec0ceb7b87bef9cf8
kiran-git25 <kiran-sai@outlook.com>
2025-08-09 08:35:44 +0530

    Update AudioPlayer.jsx

--- src/components/viewers/AudioPlayer.jsx
+++ src/components/viewers/AudioPlayer.jsx
@@ -1,34 +1,15 @@
-import React, { useRef } from "react";
+import React from "react";
 
 export default function AudioPlayer({ file }) {
-  const audioRef = useRef(null);
-
-  const handlePlay = () => {
-    if (audioRef.current) {
-      audioRef.current.play();
-    }
-  };
-
-  const handlePause = () => {
-    if (audioRef.current) {
-      audioRef.current.pause();
-    }
-  };
-
   return (
     <div style={{ textAlign: "center" }}>
       <audio
-        ref={audioRef}
-        src={file.url || file}
         controls
         style={{ width: "100%" }}
-      />
-      <div style={{ marginTop: "10px" }}>
-        <button onClick={handlePlay}>Play</button>
-        <button onClick={handlePause} style={{ marginLeft: "5px" }}>
-          Pause
-        </button>
-      </div>
+        src={file.url || file}
+      >
+        Your browser does not support the audio tag.
+      </audio>
     </div>
   );
 }
