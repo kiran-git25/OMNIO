c8bf869c5b7dd9c9bdb10d23c7624c74f5e4070e
kiran-git25 <kiran-sai@outlook.com>
2025-08-09 08:35:14 +0530

    Update VideoPlayer.jsx

--- src/components/viewers/VideoPlayer.jsx
+++ src/components/viewers/VideoPlayer.jsx
@@ -1,33 +1,15 @@
-import React, { useRef } from "react";
+import React from "react";
 
 export default function VideoPlayer({ file }) {
-  const videoRef = useRef(null);
-
-  const handleFullscreen = () => {
-    if (videoRef.current) {
-      if (videoRef.current.requestFullscreen) {
-        videoRef.current.requestFullscreen();
-      } else if (videoRef.current.webkitRequestFullscreen) {
-        videoRef.current.webkitRequestFullscreen();
-      }
-    }
-  };
-
   return (
     <div style={{ textAlign: "center" }}>
       <video
-        ref={videoRef}
-        src={file.url || file}
         controls
-        style={{
-          maxWidth: "100%",
-          maxHeight: "80vh",
-          backgroundColor: "#000",
-        }}
-      />
-      <div style={{ marginTop: "10px" }}>
-        <button onClick={handleFullscreen}>Fullscreen</button>
-      </div>
+        style={{ maxWidth: "100%", maxHeight: "80vh" }}
+        src={file.url || file}
+      >
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
