b4009506c8187947421d0f565f5f064e325101ce
kiran-git25 <kiran-sai@outlook.com>
2025-08-09 08:40:45 +0530

    Update CodeViewer.jsx

--- src/components/viewers/CodeViewer.jsx
+++ src/components/viewers/CodeViewer.jsx
@@ -1,42 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import Prism from 'prismjs';
-import 'prismjs/themes/prism.css';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-python';
+import React, { useEffect, useState } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism.css";
 
-export default function CodeViewer({ file, url }) {
-  const [code, setCode] = useState('');
-  const [query, setQuery] = useState('');
+export default function CodeViewer({ file, language = "javascript" }) {
+  const [code, setCode] = useState("");
 
   useEffect(() => {
-    const load = async () => {
-      const content = file ? await file.text() : await fetch(url).then(r => r.text());
-      setCode(content);
-      setTimeout(() => Prism.highlightAll(), 0);
-    };
-    load();
-  }, [file, url]);
+    if (file.content) {
+      setCode(file.content);
+    } else if (file.url) {
+      fetch(file.url)
+        .then((res) => res.text())
+        .then(setCode)
+        .catch((err) => console.error("Error loading code:", err));
+    }
+  }, [file]);
 
-  const highlighted = code.replace(
-    new RegExp(query, 'gi'),
-    match => `<mark>${match}</mark>`
-  );
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [code, language]);
 
   return (
-    <>
-      <input
-        type="text"
-        placeholder="Search code..."
-        value={query}
-        onChange={e => setQuery(e.target.value)}
-        style={{ marginBottom: '1rem', padding: '6px', width: '100%' }}
-      />
-      <pre className="language-javascript">
-        <code
-          className="language-javascript"
-          dangerouslySetInnerHTML={{ __html: highlighted }}
-        />
-      </pre>
-    </>
+    <pre style={{ maxHeight: "80vh", overflowY: "auto" }}>
+      <code className={`language-${language}`}>{code}</code>
+    </pre>
   );
 }
