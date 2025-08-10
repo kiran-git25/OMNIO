e45c6017f03b7c92bcbb4bc6791f593e214a10e6
kiran-git25 <kiran-sai@outlook.com>
2025-08-10 17:41:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
-import ViewerPanel from "./components/ViewerPanel";
-import ChatBox from "./components/ChatBox";
+import React from 'react';
 import TabManager from './components/TabManager';
 import './index.css';
 
-export default function App() {
-  const [activeFile, setActiveFile] = useState(null);
-
-  return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      <ViewerPanel activeFile={activeFile} />
-      <ChatBox onOpenFile={setActiveFile} />
-    </div>
-  );
-  function App() 
+function App() {
   return (
     <div className="app-container">
       <TabManager />
     </div>
   );
 }
+
 export default App;
